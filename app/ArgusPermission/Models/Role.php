2447d7e81f6cfbc1af73c6d927f4373407c92d41
Alessandro <alessandrostfr@gmail.com>
2020-07-14 13:51:13 +0200

    Creo los metodos para la relacion N a N entre user y role

--- app/ArgusPermission/Models/Role.php
+++ app/ArgusPermission/Models/Role.php
@@ -6,5 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Role extends Model
 {
-    //
+
+    //fillable propertie
+
+    protected $fillable = [
+        'name', 'slug', 'description', 'full-access',
+    ];
+
+    /*Cuando seleccionemos rol y llamemos a esta funcion automaticamente nos devolvera a todos los usuarios relacionados con este rol*/
+
+    public function users(){
+        return $this->belongsToMany('App\User')->withTimesTamps();
+    }
 }
